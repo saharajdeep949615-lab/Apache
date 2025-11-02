bb7599bc008ff2608b8eb7b9fa8af690f7e9d96d
saharajdeep949615-lab <saharajdeep949615@gmail.com>
2025-11-02 16:24:16 +0530

    second

--- src/main/java/apache1/WritingDataIntoExcel.java
+++ src/main/java/apache1/WritingDataIntoExcel.java
@@ -18,20 +18,23 @@ public class WritingDataIntoExcel {
 
         XSSFSheet sheet=workbook.createSheet();
 
-        XSSFRow row1=sheet.createRow(0);
-        row1.createCell(0).setCellValue("Java");
-        row1.createCell(1).setCellValue(1);
-        row1.createCell(2).setCellValue("Automation");
+//        XSSFRow row1=sheet.createRow(0);
+//        row1.createCell(0).setCellValue("Java");
+//        row1.createCell(1).setCellValue(1);
+//        row1.createCell(2).setCellValue("Automation");
+//
+//        XSSFRow row2=sheet.createRow(1);
+//        row2.createCell(0).setCellValue("Python");
+//        row2.createCell(1).setCellValue(2);
+//        row2.createCell(2).setCellValue("Automation");
+//
+//        XSSFRow row3=sheet.createRow(2);
+//        row3.createCell(0).setCellValue("C#");
+//        row3.createCell(1).setCellValue(3);
+//        row3.createCell(2).setCellValue("Automation");
 
-        XSSFRow row2=sheet.createRow(1);
-        row2.createCell(0).setCellValue("Python");
-        row2.createCell(1).setCellValue(2);
-        row2.createCell(2).setCellValue("Automation");
 
-        XSSFRow row3=sheet.createRow(2);
-        row3.createCell(0).setCellValue("C#");
-        row3.createCell(1).setCellValue(3);
-        row3.createCell(2).setCellValue("Automation");
+        
 
         workbook.write(file);
         workbook.close();
